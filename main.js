760db6517f62a36626bf60e29b224116d7d9bb96
Thompson Plyler <nealok.g@gmail.com>
2022-06-16 18:59:16 -0400

    Made step 2 more apparent.

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
-const body = document.getElementsByTagName('body')[0]
-const main = document.getElementsByTagName('main')[0]
-const input = document.getElementById('input')
-const myName = document.getElementById('name')
-const button = document.getElementById('button')
-const pageBg = document.getElementById('pageBg')
-const cardBg = document.getElementById('cardBg')
+// A method is a function declared
+// from an object with dot notation.
 
-pageBg.addEventListener('click', function(){
-    body.style.backgroundColor = randomColor()
-})
+// select elements on the HTML page.
+// .getElementsByTagName looks for everything on the page
+// that has an HTML tag of whatever is in the perentheses:
+const body = document.getElementsByTagName("body")[0];
+const main = document.getElementsByTagName("main")[0];
 
-cardBg.addEventListener('click', function(){
-    main.style.backgroundColor = randomColor()
-})
+// .getElementById looks for HTML elements with an "id"
+// of whatever is in the perentheses.
+const input = document.getElementById("input");
+const myName = document.getElementById("name");
+const pageBg = document.getElementById("pageBg");
 
-button.addEventListener('click', function(){
-    myName.textContent = input.value
-    input.value=""
-})
+const cardBg = document.getElementById("cardBg");
+const button = document.getElementById("button");
 
-function randomColor(){
-    const r = (Math.floor(Math.random()*255))
-    const g = (Math.floor(Math.random()*255))
-    const b = (Math.floor(Math.random()*255))
+// add click event listener to pageBg change button
+pageBg.addEventListener("click", function () {
+  body.style.backgroundColor = randomColor();
+});
 
-    return `rgba(${r}, ${g}, ${b}, 1)`
+// add click event listener to cardBg change button
+cardBg.addEventListener("click", function () {
+  main.style.backgroundColor = randomColor();
+});
+
+// add click event to "Change it" button
+button.addEventListener("click", function () {
+  myName.textContent = input.value;
+  input.value = "";
+});
+
+// This is a function.
+// Functions perform actions.
+// This function returns a random color.
+function randomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  return `rgba(${r}, ${g}, ${b}, 1)`;
 }
---
+++ notes.md
@@ -0,0 +1,5 @@
+# CSS On Page
+
+1. Inline
+2. Same page
+3. External File
